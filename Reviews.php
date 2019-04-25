0261c5722879bb0312d98f23a493e7846eb2c511
Stan Koster <stan.koster@outlook.com>
2019-04-25 11:30:15 +0200

    Update Reviews.php
    Cijfers toegevoegd

--- Reviews.php
+++ Reviews.php
@@ -13,29 +13,36 @@
 <body>
 
 <h2>Reviews</h2>
-<hr>
-<h3>Naam Achternaam</h3>
-<P>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
-    Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
-    scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
-</P>
 
 <hr>
+
 <h3>Naam Achternaam</h3>
 <P>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
     Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
     scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
 </P>
+<h3>Cijfer: 9</h3>
 
 <hr>
+
 <h3>Naam Achternaam</h3>
 <P>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
     Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
     scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
 </P>
+<h3>Cijfer: 8</h3>
+
+<hr>
+
+<h3>Naam Achternaam</h3>
+<P>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
+    Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
+    scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
+</P>
+<h3>Cijfer: 10</h3>
 
 </body>
 
