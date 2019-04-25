d6246798d046c11a8e92f8054663f0e91badf0d7
Stan Koster <stan.koster@outlook.com>
2019-04-25 11:22:58 +0200

    Reviews1.0
    Eerste commit van de reviews.

---
+++ Reviews.php
@@ -0,0 +1,46 @@
+
+<!doctype html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <title>Reviews</title>
+
+</head>
+
+<body>
+
+<h2>Reviews</h2>
+<hr>
+<h3>Naam Achternaam</h3>
+<P>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
+    Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
+    scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
+</P>
+
+<hr>
+<h3>Naam Achternaam</h3>
+<P>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
+    Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
+    scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
+</P>
+
+<hr>
+<h3>Naam Achternaam</h3>
+<P>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum vulputate metus, vitae interdum quam fringilla a.
+    Donec elementum accumsan tempus. Sed a malesuada felis. Aliquam erat volutpat. Donec sed vulputate mauris. Quisque maximus
+    scelerisque quam ut dictum. Fusce et eros mi. Mauris vestibulum scelerisque felis, non vestibulum dolor vestibulum ac.
+</P>
+
+</body>
+
+</html>
+
+<?php
+
+?>
