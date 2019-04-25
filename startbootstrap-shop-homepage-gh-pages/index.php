f6f211dedf026b38207d9428796d6fbf728d58af
joristelintelo <joristelintelo@live.nl>
2019-04-25 11:49:06 +0200

    Database connectie

--- startbootstrap-shop-homepage-gh-pages/index.php
+++ startbootstrap-shop-homepage-gh-pages/index.php
@@ -1,16 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "chaingang";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once("includes/database.php");
 ?>
+
 
 <!DOCTYPE html>
 
