53a5ce748e3577a8d633c1480c31ce9a0a04c6a0
Stan Koster <stan.koster@outlook.com>
2019-06-05 11:36:52 +0200

    Product pagina
    Product pagina 1.0

---
+++ product-pagina/index.php
@@ -0,0 +1,159 @@
+<?php
+require_once("includes/database.php");
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Homepage | Chain Gang</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Chain Gang</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="contact-pagina/index.php">Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Winkelwagen</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Product
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-lg-3">
+
+            <h5 class="my-4"><img src="http://rijwielw-2.rijwielwebshop.nl/wp-content/uploads/2017/03/Fluit-logo.jpg" height="150" width="250" </h5>
+            <div class="list-group">
+                <a href="#" class="list-group-item">Mannenfietsen</a>
+                <a href="#" class="list-group-item">Vrouwenfietsen</a>
+                <a href="#" class="list-group-item">Kinderfietsen</a>
+            </div>
+
+        </div>
+
+        <div class="col-lg-9">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-lg-12">
+                        <h1>Fiets naam</h1>
+                    </div>
+
+                </div>
+                
+                <div class="row">
+
+                    <div class="col-lg-6">
+
+                    <img src="123.jfif">
+
+                    </div>
+
+                    <div class="col-lg-6">
+
+                        <h1>€1000,-</h1>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+                            pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede
+                            justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus
+                            ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis
+                            pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.
+                            Aenean vulputate eleifend tellus.</p>
+                        <hr>
+                    </div>
+                    
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-6">
+
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                        parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+                        pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede
+                        justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+                        <hr>
+
+                    </div>
+
+                    <div class="col-lg-6">
+
+                        <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Toevoegen aan winkelwagen</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Chain Gang Gang 2019</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+
